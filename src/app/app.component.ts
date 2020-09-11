52f76a9969d0a4a885d27d3f1ba44e258bd68a8b
Master <git@wbsoft.pl>
2020-09-12 00:29:23 +0200

    Po spięciu Angulara ze SpringBootem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MedSerwis';
+  public alaData = 'Ala ma kota';
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ZdarzenieComponent } from './component/zdarzenia/zdarzenie.component';
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ZdarzenieComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
